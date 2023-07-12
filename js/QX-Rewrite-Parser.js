37096aba12d0ae3a1543f0d6f1b585deb1207ea4
love796-QAQ <80121684+love796-QAQ@users.noreply.github.com>
2023-07-12 13:01:27 +0800

    Add files via upload

--- js/QX-Rewrite-Parser.js
+++ js/QX-Rewrite-Parser.js
@@ -255,6 +255,12 @@ var desc = "";
 var icon = "";
 var rewriteName = req.substring(req.lastIndexOf('/') + 1).split('.')[0];
 
+//查询js binarymode相关
+let binaryInfo = $persistentStore.read("Parser_binary_info");
+if (binaryInfo != null && binaryInfo != "") {
+	binaryInfo = JSON.parse(binaryInfo);
+} else { binaryInfo = []; };
+
 //缓存有效期相关
 var currentTime = new Date();
 var seconds = Math.floor(currentTime.getTime() / 1000); // 将毫秒转换为秒
@@ -617,7 +623,7 @@ console.log("插件图标：" + pluginIcon);
 
 						if ((isSurgeiOS || isLooniOS || isShadowrocket) && proto == "true") {
 							proto = ", binary-body-mode=true";
-						};
+						} else if ((isSurgeiOS || isLooniOS || isShadowrocket) && proto == "false") { proto = ""; };
 
 						if (isStashiOS) {
 
@@ -1063,16 +1069,36 @@ function parseQueryString(url) {
 	return params;
 };
 
+
 async function isBinaryMode(url) {
+
 	if (url.search(/proto/i) != -1) {
 		return "true"
 	} else if (url.search(/(tieba|youtube|bili|spotify)/i) != -1) {
-		const res = await http(url);
-		if (res == undefined) {
-			console.log("Script Hub QX 转换器 查询脚本链接失败");
-			return "false";
-		} else if (res.includes(".bodyBytes")) {
-			return "true";
-		} else { return "false"; }
-	} else { return "false" };
+		if (binaryInfo != "" && binaryInfo.some(item => item.url === url)) {
+			for (let i = 0; i < binaryInfo.length; i++) {
+				if (binaryInfo[i].url === url) {
+					binarymode = binaryInfo[i].binarymode;
+					return binarymode;
+					break;
+				}
+			}
+		} else {
+			const res = await http(url);
+			if (res == undefined) {
+				console.log("Script Hub QX 转换器 查询脚本链接失败");
+				return "false";
+			} else if (res.includes(".bodyBytes")) {
+				binaryInfo.push({ "url": url, "binarymode": "true" });
+				$persistentStore.write(JSON.stringify(binaryInfo), "Parser_binary_info")
+				return "true";
+			} else {
+				binaryInfo.push({ "url": url, "binarymode": "false" });
+				$persistentStore.write(JSON.stringify(binaryInfo), "Parser_binary_info")
+				return "false";
+			}
+		}//没有信息或者没有url的信息
+
+	} else { return "false" }
+
 }
